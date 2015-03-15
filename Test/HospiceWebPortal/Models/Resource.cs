11096b15ccdc5a5d326ccccdff86522fd75e5b0b
Kevin Allan <kevinaln@gmail.com>
2015-03-15 16:11:13 -0400

    Rescaffold Resource. Added upload functionality.

--- Test/HospiceWebPortal/Models/Resource.cs
+++ Test/HospiceWebPortal/Models/Resource.cs
@@ -10,16 +10,16 @@ namespace HospiceWebPortal.Models
     {
         public int ID { get; set; }
         public string Description { get; set; }
-        public string Name { get; set; }
-        public string CreatedOn { get; set; }
+        [Required(ErrorMessage = "You cannot leave the name of the file blank.")]
+        [StringLength(100, ErrorMessage = "The name of the file cannot be more than 100 characters")]
+        public string FileName { get; set; }
+        public DateTime CreatedOn { get; set; }
         [Required]
         public byte[] FileContent { get; set; }
         [Required]
         [StringLength(256)]
         public string MimeType { get; set; }
-        [Required(ErrorMessage="You cannot leave the name of the file blank.")]
-        [StringLength(100, ErrorMessage = "The name of the file cannot be more than 100 characters")]
-        public string FileName { get; set; }
+        
     }
     public class ResourceViewModel
     {
